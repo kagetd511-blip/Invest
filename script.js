d255c72560d9aa3a36e9a663854f8a62b7db839b
kagetd511-blip <kagetd511@gmail.com>
2026-06-13 10:13:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const menuBtn = document.getElementById("menuBtn");
+const drawer = document.getElementById("drawer");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+    drawer.classList.add("active");
+    overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+    drawer.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+
+}, 3000);
