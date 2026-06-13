83f1164b4a567bd033795aefa4b17d312efd33c7
kagetd511-blip <kagetd511@gmail.com>
2026-06-14 01:15:59 +0700

    Create register.js

---
+++ register.js
@@ -0,0 +1,89 @@
+function register(){
+
+let phone =
+document.getElementById("phone").value.trim();
+
+let email =
+document.getElementById("email").value.trim();
+
+let password =
+document.getElementById("password").value;
+
+let confirmPassword =
+document.getElementById("confirmPassword").value;
+
+let referral =
+document.getElementById("referral").value.trim();
+
+if(phone === ""){
+
+    alert(
+    "Nomor HP wajib diisi"
+    );
+
+    return;
+
+}
+
+if(password === ""){
+
+    alert(
+    "Sandi wajib diisi"
+    );
+
+    return;
+
+}
+
+if(password !== confirmPassword){
+
+    alert(
+    "Ulangi sandi tidak cocok"
+    );
+
+    return;
+
+}
+
+if(localStorage.getItem(phone)){
+
+    alert(
+    "Nomor HP sudah terdaftar"
+    );
+
+    return;
+
+}
+
+let userData = {
+
+    phone: phone,
+
+    email: email,
+
+    password: password,
+
+    referral: referral,
+
+    saldo: 0,
+
+    level: "Member",
+
+    registerDate:
+    new Date().toLocaleDateString()
+
+};
+
+localStorage.setItem(
+    phone,
+    JSON.stringify(userData)
+);
+
+alert(
+"Pendaftaran berhasil"
+);
+
+window.location.href =
+"index.html";
+
+}
