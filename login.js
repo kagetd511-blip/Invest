526a3d54abafd14f3b5116d6c198c422027264fc
kagetd511-blip <kagetd511@gmail.com>
2026-06-16 03:44:54 +0700

    Update login.js

--- login.js
+++ login.js
@@ -84,15 +84,19 @@ function login(){
         );
 
 function showLoading(){
+
     document
     .getElementById("loadingScreen")
     .classList.add("active");
+
 }
 
 function hideLoading(){
+
     document
     .getElementById("loadingScreen")
     .classList.remove("active");
+
 }
 
 function goRegister(){
@@ -102,16 +106,60 @@ function goRegister(){
     .classList.add("active");
 
     setTimeout(()=>{
+
         window.location.href =
         "register.html";
+
     },3000);
 
 }
 
 function login(){
 
-    ...
-    
+    let phone =
+    document.getElementById("phone")
+    .value.trim();
+
+    let password =
+    document.getElementById("password")
+    .value;
+
+    if(phone === ""){
+
+        showAlert(
+        "Peringatan",
+        "Masukkan Nomor HP"
+        );
+
+        return;
+    }
+
+    if(password === ""){
+
+        showAlert(
+        "Peringatan",
+        "Masukkan Sandi"
+        );
+
+        return;
+    }
+
+    let data =
+    localStorage.getItem(phone);
+
+    if(!data){
+
+        showAlert(
+        "Login Gagal",
+        "Akun belum terdaftar"
+        );
+
+        return;
+    }
+
+    let user =
+    JSON.parse(data);
+
     if(user.password === password){
 
         localStorage.setItem(
@@ -127,13 +175,11 @@ function login(){
         showLoading();
 
         setTimeout(()=>{
+
             window.location.href =
             "dashboard.html";
+
         },5000);
-
-    }
-
-}
 
     }else{
 
