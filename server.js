bb1eceb5487677cebd978763d2863b2278abfff7
kagetd511-blip <kagetd511@gmail.com>
2026-06-18 15:37:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ setInterval(async ()=>{
 // =========================
 // SERVER START
 // =========================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, ()=>{
     console.log("Server running on", PORT);
 });
