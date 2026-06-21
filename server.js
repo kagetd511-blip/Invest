9f87811b4f410d78af9ab4a42a98b8a95a7db879
kagetd511-blip <kagetd511@gmail.com>
2026-06-21 17:28:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({
+    limit:"50mb"
+}));
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"50mb"
+}));
 
 // =========================
 // TELEGRAM BOT
